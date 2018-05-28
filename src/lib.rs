00ecbaa8702b0e538bf3650bbd0002b8e2ed2a76
olivia <olivia@fastmail.com>
2018-05-28 14:03:58 +0200

    add optional arguments for source and destination

--- src/lib.rs
+++ src/lib.rs
@@ -15,18 +15,20 @@ pub struct SimpleFile {
 
 type MiddlewareFunction = Box<FnMut(&mut Vec<SimpleFile>)>;
 
-pub fn seven(middleware: Vec<MiddlewareFunction>, destination: &str) -> Vec<SimpleFile> {
+pub fn seven(middleware: Vec<MiddlewareFunction>, source: Option<&str>, destination: Option<&str>) -> Vec<SimpleFile> {
+    let f_source = source.unwrap_or(".");
+    let f_dest = destination.unwrap_or("_site");
     let mut files = Vec::<SimpleFile>::new();
-    read_dir(&mut files);
+    read_dir(&mut files, f_source);
     for mut function in middleware {
         function(&mut files);
     }
-    write_dir(&mut files);
+    write_dir(&mut files, f_source, f_dest);
     files
 }
 
-fn read_dir(files: &mut Vec<SimpleFile>) {
-    for entry in WalkDir::new("example") {
+fn read_dir(files: &mut Vec<SimpleFile>, source: &str) {
+    for entry in WalkDir::new(source) {
         let entry = entry.unwrap();
         let path = entry.path().to_owned();
         if !&path.is_dir() {
@@ -34,7 +36,7 @@ fn read_dir(files: &mut Vec<SimpleFile>) {
             let mut content = String::new();
             file.read_to_string(&mut content).unwrap();
             let file_struct = SimpleFile {
-                content: content,
+                content,
                 abs_path: path.clone().canonicalize().unwrap(),
                 rel_path: path,
             };
@@ -43,10 +45,10 @@ fn read_dir(files: &mut Vec<SimpleFile>) {
     }
 }
 
-fn write_dir(files: &mut Vec<SimpleFile>) {
+fn write_dir(files: &mut Vec<SimpleFile>, source: &str, destination: &str) {
     for file in files {
-        let temp_path = file.rel_path.strip_prefix("example").unwrap();
-        let destination_path = PathBuf::from("destination").join(temp_path);
+        let temp_path = file.rel_path.strip_prefix(source).unwrap();
+        let destination_path = PathBuf::from(destination).join(temp_path);
         let mut dir_path = destination_path.clone();
         dir_path.pop();
         DirBuilder::new().recursive(true).create(&dir_path).unwrap();
@@ -70,7 +72,7 @@ mod tests {
                 let file: &mut SimpleFile = &mut files[0];
                 file.content = "override".to_string();
             }),
-        ]);
+        ], Some("example"), None);
         assert_eq!(result[0].content, "override");
     }
 }
