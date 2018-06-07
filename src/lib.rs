d1d2ebd10e7ef4c47922b418978c60b40a189652
olivia <olivia@fastmail.com>
2018-06-07 19:55:22 +0200

    document the crate

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,40 @@
+//! `nya` is a simple file processor. It's extremely simple, very fast, and super
+//! cute, as well. It reads files from a directory, does stuff to them, and then
+//! spits them out somewhere else. That's all it does. The "stuff" that's being
+//! done to the files is called _middleware_. Middleware is just a function that
+//! takes a struct representing a file and then does something to that file.
+//! It doesn't even have to return it! That's how easy it is to use `nya`. Can
+//! you believe it?
+//!
+//! In its simplest form, you'd use it somewhat like this:
+//!
+//! ```
+//! extern crate nya;
+//!
+//! use nya::create_middleware;
+//!
+//! fn main() {
+//!     nya::run(vec![
+//!         create_middleware(|files| {
+//!             let file = &mut files[0];
+//!             file.content = "test hello".to_string();
+//!         }, Some("source"), Some("destination"))
+//!     ]).unwrap()
+//! }
+//! ```
+//!
+//! ### How does `nya` compare to other software?
+//!
+//! At some distant point in time, I'd like for `nya` to be used as a static site
+//! generator. How would it compare to other existing static site generators, then?
+//! Well, it's much simpler than __Jekyll__ or __Hugo__, for example, and it's even
+//! simpler than other JavaScript-based SSGs such as __Metalsmith__ or __Wintersmith__.
+//! Existing Rust SSGs have all more or less tried to replicate the full feature set
+//! of Jekyll or comparable software, so I hope I'll be scratching an itch here.
+//!
+//! `nya` currently only depends on `walkdir`, and I'd like to keep dependencies
+//! as light as possible.
+
 extern crate walkdir;
 
 use std::fs::DirBuilder;
@@ -7,19 +44,76 @@ use std::io::prelude::*;
 use std::path::PathBuf;
 use walkdir::WalkDir;
 
+/// A struct describing a simple file, with only a name, content, and its path.
+///
+/// # Examples
+///
+/// ```
+/// let file = nya::SimpleFile {
+///     name: OsString::from("coolfile.txt"),
+///     content: "hello".to_string(),
+///     abs_path: PathBuf::from(r"/home/coolfile.txt"),
+///     rel_path: PathBuf::from(r"coolfile.txt"),
+/// }
+/// ```
 #[derive(Debug)]
 pub struct SimpleFile {
+    /// The filename, as an `OsString`.
     pub name: OsString,
+    /// The content of the file, as an owned `String`.
     pub content: String,
+    /// The absolute path of the file, as a `PathBuf`.
     pub abs_path: PathBuf,
+    /// The relative path of the file, as a `PathBuf`.
     pub rel_path: PathBuf,
 }
 
 type MiddlewareFunction = Box<FnMut(&mut Vec<SimpleFile>)>;
 
+/// A convenience function that creates middleware.
+///
+/// Takes a closure and boxes it so it can be
+/// (more or less) safely added to the middleware chain and so that we don't
+/// have to worry about lifetimes. This closure itself takes a `files` argument
+/// that is a `Vec` of `SimpleFile`s. This Vec can be modified in place and
+/// doesn't have to be returned.
+///
+/// # Example
+///
+/// ```
+/// let func = nya::create_middleware(|files| {
+///     let file = files[0];
+///     file.content = "haha hello";
+/// }
+///
+/// nya::run(vec![func]).unwrap();
+/// ```
 pub fn create_middleware<T>(x: T) -> Box<T> {
     Box::new(x)
 }
+
+/// Runs a middleware chain.
+///
+/// Reads a file from the `source` argument (default is the
+/// current directory), runs the collected files through the provided middleware
+/// chain, and then writes the result to the `destination` argument (default
+/// is `_site`). Both arguments _have_ to be provided, although they accept
+/// an `Option`, so you can pass in `None` if you want the defaults to apply.
+/// Returns a `Result<Vec<SimpleFile>, std::io::Error>`.
+///
+/// # Example
+///
+/// ```
+/// let func = nya::create_middleware(|files| {
+///     let file = files[0];
+///     file.content = "haha hello";
+/// }
+///
+/// let result = nya::run(vec![func]);
+/// if let Ok(r) = result {
+///     println!("Success!");
+/// }
+/// ```
 
 pub fn run(
     middleware: Vec<MiddlewareFunction>,
